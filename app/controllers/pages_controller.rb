f35ab57ff2433d2bdd46a1423dc6d23453ab9ac3
cmayfi1 <cmayfi1@gmail.com>
2014-08-04 10:00:26 -0400

    changes to blog stylesheets

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def sitemap
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/sitemap'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
